6ee7d0374f1ae2afc6b9066339014c34d1c1df84
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-09-14 03:43:34 +1000

    Added publish script

---
+++ publish.cmd
@@ -0,0 +1,7 @@
+robocopy .\www\ ..\JamesHurburgh.github.io\idle-game\ /s
+
+del ..\JamesHurburgh.github.io\idle-game\index.html
+
+del ..\JamesHurburgh.github.io\idle-game\idle-game-1.html
+
+move ..\JamesHurburgh.github.io\idle-game\idle-game-2.html ..\JamesHurburgh.github.io\idle-game\index.html
