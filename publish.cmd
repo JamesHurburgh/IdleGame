92952a8972ef1ee5a70fe76294f60c474db05faa
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-10-04 08:40:00 +1000

    Archived old code Moved modals to seperate files to aid maintenance.

--- publish.cmd
+++ publish.cmd
@@ -1,7 +1,1 @@
 robocopy .\www\ ..\JamesHurburgh.github.io\idle-game\ /s
-
-del ..\JamesHurburgh.github.io\idle-game\index.html
-
-del ..\JamesHurburgh.github.io\idle-game\idle-game-1.html
-
-move ..\JamesHurburgh.github.io\idle-game\idle-game-2.html ..\JamesHurburgh.github.io\idle-game\index.html
