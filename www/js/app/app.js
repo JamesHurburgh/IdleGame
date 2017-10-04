f844f2b640c221dbfddb28937225ae69c4c8f199
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-10-04 12:34:43 +1000

    Abstracted all web content parts into their own page

--- www/js/app/app.js
+++ www/js/app/app.js
@@ -17,6 +17,13 @@ requirejs.config({
 requirejs(['jquery', 'vue', 'alertify', 'store', 'chance', 'app/AdventurersGame'],
     function($, Vue, alertify, store, chance, AdventurersGame) {
 
+        function loadWebComponent(name, path){
+
+            $.get("./" + path + name + ".html", function(content) {
+                $("#"+name).replaceWith(content);
+            });
+        }
+
         function uuidv4() {
             return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
                 (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
