9d4c9b08fa8726d401c84c00baddc22e5c90add0
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-10-08 00:06:36 +1000

    Updated hosted filename

--- host.cmd
+++ host.cmd
@@ -1,3 +1,3 @@
 start ws -d www
-start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://192.168.72.1:8000/idle-game-2.html
-start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://127.0.0.1:8000/idle-game-2.html
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://192.168.72.1:8000
+start "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" http://127.0.0.1:8000
