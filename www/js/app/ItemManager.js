30331da108dff7d33c9eb4f648ebd913464aab30
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-10-11 17:38:07 +1000

    Removed unused functions

--- www/js/app/ItemManager.js
+++ www/js/app/ItemManager.js
@@ -7,20 +7,6 @@ define([
         CommonFunctions,
         items) {
 
-        function uuidv4() {
-            return ([1e7] + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, c =>
-                (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-            );
-        }
-
-        function clone(object) {
-            try {
-                return JSON.parse(JSON.stringify(object));
-            } catch (exception) {
-                log("Unable to parse object: " + JSON.stringify(object));
-            }
-        }
-
         return function ItemManager(gameState) {
 
             this.gameState = gameState;
