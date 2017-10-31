db4d23b72c9ec39b5611eed8fc643127fe9ac36c
James Hurburgh <onegodoneloveoneway@gmail.com>
2017-10-31 13:27:18 +1000

    Fixed tasks now injurying adventurers (again)

--- www/js/app/QuestManager.js
+++ www/js/app/QuestManager.js
@@ -1,9 +1,9 @@
 /*jshint esversion: 6 */
 
 define(["chance",
-    "app/CommonFunctions",
-    "json!data/contracts.json"
-],
+        "app/CommonFunctions",
+        "json!data/contracts.json"
+    ],
     function QuestManager(
         chance,
         CommonFunctions,
@@ -16,25 +16,25 @@ define(["chance",
             this.gameState = gameStateP;
             this.gameController = gameController;
 
-            this.getCompletedQuests = function () {
+            this.getCompletedQuests = function() {
                 if (!this.gameState.completedQuests) this.gameState.completedQuests = [];
                 return this.gameState.completedQuests;
             };
 
-            this.getRunningQuests = function () {
+            this.getRunningQuests = function() {
                 if (!gameState.runningQuests) gameState.runningQuests = [];
                 return gameState.runningQuests;
             };
 
-            this.showQuestsTab = function () {
+            this.showQuestsTab = function() {
                 return this.getCompletedQuests().length + this.getRunningQuests().length !== 0;
             };
 
-            this.getSelectedContract = function () {
+            this.getSelectedContract = function() {
                 return this.gameState.selectedContract;
             };
 
-            this.selectNextContract = function () {
+            this.selectNextContract = function() {
                 var availableContracts = this.gameController.LocationManager().getCurrentLocation().availableContracts;
                 var selectedContract = this.getSelectedContract();
                 var index = 0;
@@ -44,30 +44,30 @@ define(["chance",
                 this.gameState.selectedContract = availableContracts[index];
             };
 
-            this.canSendSelectedQuest = function () {
+            this.canSendSelectedQuest = function() {
                 return this.canSendQuest(this.getSelectedContract());
             };
 
-            this.isSkillRelevantToContract = function (skill, contract) {
+            this.isSkillRelevantToContract = function(skill, contract) {
                 if (!skill || !contract) return false;
                 var skillRequiredForContract = contract.requirements.attributes.filter(sk => sk.type == skill.name)[0];
                 return skillRequiredForContract !== undefined && skillRequiredForContract.amount > 0;
             };
 
-            this.rejectSelectedContract = function () {
+            this.rejectSelectedContract = function() {
                 this.rejectContract(this.getSelectedContract());
                 this.gameState.selectedContract = null;
                 this.selectNextContract();
             };
 
-            this.rejectContract = function (contract) {
+            this.rejectContract = function(contract) {
                 var availableContracts = this.gameController.LocationManager().getCurrentLocation().availableContracts;
                 availableContracts.splice(availableContracts.indexOf(contract), 1);
                 this.gameController.StatisticsManager().trackStat("reject", "contract", 1);
                 this.gameController.StatisticsManager().trackStat("reject", contract.name, 1);
             };
 
-            this.canSendQuest = function (contract) {
+            this.canSendQuest = function(contract) {
                 if (!contract) return;
                 var adventurerManager = this.gameController.AdventurerManager();
                 if (adventurerManager.getCurrentParty().length < 1) {
@@ -80,7 +80,7 @@ define(["chance",
                 // }, true);
             };
 
-            this.getSuggestedSkills = function (contract) {
+            this.getSuggestedSkills = function(contract) {
                 if (!contract) return;
                 var skills = [];
                 if (!contract.tasks || contract.tasks.length === 0) {
@@ -103,11 +103,11 @@ define(["chance",
                 return skills;
             };
 
-            this.getCurrentQuestRequiredAndAssignedSkillCount = function (skillName) {
+            this.getCurrentQuestRequiredAndAssignedSkillCount = function(skillName) {
                 return this.getRequiredAndAssignedSkillCount(this.getSelectedContract(), skillName);
             };
 
-            this.getRequiredAndAssignedSkillCount = function (contract, skillName) {
+            this.getRequiredAndAssignedSkillCount = function(contract, skillName) {
                 if (!contract || !skillName) return;
                 var currentlyAssigned = this.gameController.AdventurerManager().getCurrentPartyAttribute(skillName);
                 if (currentlyAssigned === 0) return 0;
@@ -118,23 +118,23 @@ define(["chance",
                 return Math.min(currentlyAssigned, requiredSkill.amount);
             };
 
-            this.getCurrentQuestRequiredAndUnassignedSkillCount = function (skillName) {
+            this.getCurrentQuestRequiredAndUnassignedSkillCount = function(skillName) {
                 return this.getRequiredAndUnassignedSkillCount(this.getSelectedContract(), skillName);
             };
 
-            this.getRequiredAndUnassignedSkillCount = function (contract, skillName) {
+            this.getRequiredAndUnassignedSkillCount = function(contract, skillName) {
                 var currentlyAssigned = this.gameController.AdventurerManager().getCurrentPartyAttribute(skillName);
                 var requiredSkill = contract.requirements.attributes.filter(skill => skill.type == skillName)[0];
                 if (!requiredSkill) return 0;
                 return Math.max(requiredSkill.amount - currentlyAssigned, 0);
             };
 
-            this.sendSelectedQuest = function () {
+            this.sendSelectedQuest = function() {
                 this.sendQuest(this.getSelectedContract());
                 this.selectNextContract();
             };
 
-            this.sendQuest = function (contract) {
+            this.sendQuest = function(contract) {
                 if (!this.canSendQuest(contract)) {
                     return;
                 }
@@ -160,7 +160,7 @@ define(["chance",
 
                 this.getRunningQuests().push(quest);
 
-                this.getRunningQuests().sort(function (a, b) {
+                this.getRunningQuests().sort(function(a, b) {
                     return a.expires - b.expires;
                 });
 
@@ -171,7 +171,7 @@ define(["chance",
 
             // Expediations
 
-            this.claimAllCompletedQuests = function () {
+            this.claimAllCompletedQuests = function() {
                 // while (this.completedExpeditions.length > 0) {
                 //     if (this.completedExpeditions[0].success) {
                 //         this.claimReward(this.completedExpeditions[0]);
@@ -181,20 +181,20 @@ define(["chance",
                 // }
             };
 
-            this.removeQuest = function (expedition) {
+            this.removeQuest = function(expedition) {
                 this.getCompletedQuests().splice(this.getCompletedQuests().indexOf(expedition), 1);
             };
 
-            this.questProgress = function (expedition) {
+            this.questProgress = function(expedition) {
                 return 100 * ((Date.now() - expedition.start) / (expedition.expires - expedition.start));
             };
 
-            this.isTaskNewDay = function (quest, task) {
+            this.isTaskNewDay = function(quest, task) {
                 var index = quest.tasks.indexOf(task);
                 return index === 0 || !this.gameController.TimeManager().isSameDay(quest.tasks[index].startTime, quest.tasks[index - 1].startTime);
             };
 
-            this.checkForCompletedQuests = function () {
+            this.checkForCompletedQuests = function() {
 
                 var expired = this.getRunningQuests().filter(quest => quest.expires !== null && quest.expires <= Date.now());
 
@@ -206,19 +206,19 @@ define(["chance",
                 var questsWithCompletedTasks = this.getRunningQuests().filter(quest =>
                     quest.tasks.filter(task => task.status == "in-progress" && task.finishes <= Date.now()).length > 0);
 
-                questsWithCompletedTasks.forEach(function (quest) {
+                questsWithCompletedTasks.forEach(function(quest) {
                     this.completeTasks(quest);
                 }, this);
             };
 
-            this.completeTasks = function (quest) {
+            this.completeTasks = function(quest) {
                 var completedTasks = quest.tasks.filter(task => task.status == "in-progress" && task.finishes <= Date.now());
-                completedTasks.forEach(function (task) {
+                completedTasks.forEach(function(task) {
                     this.completeTask(quest, task);
                 }, this);
             };
 
-            this.completeTask = function (quest, task) {
+            this.completeTask = function(quest, task) {
                 task.status = "complete";
 
                 if (!task.partySkill) {
@@ -232,7 +232,7 @@ define(["chance",
                 var partyRoll = (Math.random() * task.partySkill);
                 var taskRoll = (Math.random() * task.difficulty);
                 // Check for injuries
-                var isInjured = task.injuryType !== undefined && (partyRoll >= taskRoll);
+                var isInjured = task.injuryType !== undefined && (partyRoll < taskRoll);
 
                 if (isInjured) {
                     var adventurer = chance.pickone(quest.party);
@@ -282,7 +282,7 @@ define(["chance",
 
             };
 
-            this.finishQuest = function (quest, finishes) {
+            this.finishQuest = function(quest, finishes) {
                 if (!finishes) finishes = Date.now();
                 log("completeQuest");
                 var contract = quest.contract;
@@ -297,7 +297,7 @@ define(["chance",
 
                 quest.success = quest.party.length > 0 && quest.tasks.filter(task => task.required && !task.success).length === 0;
                 quest.survivors = [];
-                quest.party.forEach(function (survivor) {
+                quest.party.forEach(function(survivor) {
                     quest.survivors.push({ adventurer: survivor });
                     if (!survivor.questLog) survivor.questLog = [];
                     survivor.questLog.push(quest.id);
@@ -384,7 +384,7 @@ define(["chance",
                 this.getCompletedQuests().push(quest);
             };
 
-            this.completeQuest = function (quest, finishes) {
+            this.completeQuest = function(quest, finishes) {
                 if (!finishes) finishes = Date.now();
                 log("completeQuest");
                 var contract = quest.contract;
@@ -401,7 +401,7 @@ define(["chance",
                 quest.survivors = [];
                 quest.casualaties = [];
                 if (quest.party) {
-                    quest.party.forEach(function (adventurer) {
+                    quest.party.forEach(function(adventurer) {
                         // Did they die?
                         if (Math.random() * this.gameController.EffectsManager().getGlobalValue("questRisk") < contract.risk) {
                             var causeOfInjury = "Injured while on contract: " + contract.name;
@@ -506,7 +506,7 @@ define(["chance",
                 this.getCompletedQuests().push(quest);
             };
 
-            this.startTask = function (quest, taskId, startTime, attempt) {
+            this.startTask = function(quest, taskId, startTime, attempt) {
                 if (!startTime) startTime = Date.now();
                 if (!attempt) attempt = 1;
 
